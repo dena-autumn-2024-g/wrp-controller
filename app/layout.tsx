cb3e50eb49113dd4849a97d6ca7559f080d18745
YukaChoco <sugar.ore.yk@gmail.com>
2024-10-26 12:11:38 +0900

    日本語に設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={`${JaroFont.className} ${DotGothic16Font.className}`}>
         {children}
       </body>
