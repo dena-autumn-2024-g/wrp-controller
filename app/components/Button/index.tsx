a60b2072adb122a99434c60f45242bc37a55953f
YukaChoco <sugar.ore.yk@gmail.com>
2024-10-26 14:41:53 +0900

    ボタンに縁を表示

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -3,10 +3,13 @@ import styles from "./component.module.css";
 
 export default function Button({ onClick }: { onClick: () => void }) {
   return (
-    <button className={styles.button} onClick={onClick}>
-      <div className={styles.topCircle} />
-      <div className={styles.middleBox} />
-      <div className={styles.bottomCircle} />
-    </button>
+    <div className={styles.container}>
+      <button className={styles.button} onClick={onClick}>
+        <div className={styles.topCircle} />
+        <div className={styles.middleBox} />
+        <div className={styles.bottomCircle} />
+      </button>
+      <div className={styles.base}></div>
+    </div>
   );
 }
